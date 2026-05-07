2ad71514999cabdd89e12921ff99103175823c21
RappCL <71191915+RappCL@users.noreply.github.com>
2026-05-07 00:04:56 -0400

    Ajuste

--- TiendaManga/TiendaManga/TiendaManga/src/main/java/TiendaManga/Model/Inventario.java
+++ TiendaManga/TiendaManga/TiendaManga/src/main/java/TiendaManga/Model/Inventario.java
@@ -1,7 +1,5 @@
 package TiendaManga.Model;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- TiendaManga/TiendaManga/TiendaManga/src/main/java/TiendaManga/Model/Pago.java
+++ TiendaManga/TiendaManga/TiendaManga/src/main/java/TiendaManga/Model/Pago.java
@@ -1,17 +1,26 @@
 package TiendaManga.Model;
 
 import java.time.LocalDate;
-import java.util.List;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Pagos")
 public class Pago {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
