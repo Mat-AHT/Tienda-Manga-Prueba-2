f9108e579cbd8fc4fd85dd45250c74883cf32fd7
Mat-AHT <mat.hormaeche@duocuc.cl>
2026-05-07 16:43:47 -0400

    inventario actualizado
    el inventario esta listo faltan el genero y el origen

--- TiendaManga/TiendaManga/TiendaManga/src/main/java/TiendaManga/Service/InventarioService.java
+++ TiendaManga/TiendaManga/TiendaManga/src/main/java/TiendaManga/Service/InventarioService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import TiendaManga.Model.Inventario;
 import TiendaManga.Repository.InventarioRepository;
-import TiendaManga.Repository.MangaRepository;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -15,18 +14,42 @@ import jakarta.transaction.Transactional;
 public class InventarioService {
 
     @Autowired
-    private InventarioRepository inventarioRepository;
-
-    @Autowired
-    private MangaRepository mangaRepository;
+    private InventarioRepository invRepository;
 
     public List<Inventario> obtenerTodos(){
-        return inventarioRepository.findAll();
+        return invRepository.findAll();
     }
 
     public Inventario buscarInventario(Integer id){
-        return 
+        return invRepository.findById(id).orElseThrow(() -> new RuntimeException("El manga no existe en el inventario"));
     }
+
+    public Inventario guardarInventario(Inventario inventario){
+        return invRepository.save(inventario);
+    }
+
+    public String eliminarInventario(Integer id){
+        try{
+            Inventario inventario = invRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("El inventario con el ID"+ id +"no existe"));
+            invRepository.delete(inventario);
+            return "El inventario ha sido retirado";
+        }catch (RuntimeException e){
+            return e.getMessage();
+        }
+
+    }
+
+    public Inventario actualizarInventario(Integer id_inventario, Inventario invActualizado){
+            Inventario inventario = invRepository.findById(id_inventario).orElseThrow(() -> new RuntimeException("No se ha encontrado el invemtario con la ID " + id_inventario));
+            if(inventario.getStock() != null){
+                inventario.setStock(invActualizado.getStock());
+            }
+            if(inventario.getBodega() != null){
+                inventario.setBodega(invActualizado.getBodega());
+            }
+            return invRepository.save(inventario);
+        }
 
     
 }
