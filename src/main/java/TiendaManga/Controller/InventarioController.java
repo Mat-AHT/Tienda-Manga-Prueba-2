92262c7ee0b065ec01cceec9c3e7385fef15ea6c
Mat-AHT <mat.hormaeche@duocuc.cl>
2026-05-10 01:09:24 -0400

    requestmapping
    el requestmapping se borro y se agrego de vuelta

--- src/main/java/TiendaManga/Controller/InventarioController.java
+++ src/main/java/TiendaManga/Controller/InventarioController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import TiendaManga.DTO.InventarioDTO;
@@ -18,6 +19,7 @@ import TiendaManga.Model.Inventario;
 import TiendaManga.Service.InventarioService;
 //hola ahi esta el cambio
 @RestController
+@RequestMapping("/api/v1/inventarios")
 public class InventarioController {
     
     @Autowired
