3a62b9ba0bf04410f4d18684917907ab98ebc1f1
RappCL <71191915+RappCL@users.noreply.github.com>
2026-05-10 20:10:49 -0400

    Fix
    se agrega @Id a modelo de usuario

--- src/main/java/TiendaManga/Model/Usuario.java
+++ src/main/java/TiendaManga/Model/Usuario.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -18,6 +19,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "USUARIO")
 public class Usuario {
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_usuario;
 
